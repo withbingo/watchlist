5d488e89b9ad41b5b6b5d56a0c144c71627ac030
Bingo <withbingo@126.com>
2021-07-25 16:37:33 +0800

    Add static files

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, url_for, render_template
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 name = 'Grey Li'
@@ -16,26 +15,10 @@ movies = [
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
 
+
 @app.route('/')
 def index():
     return render_template('index.html', name=name, movies=movies)
-
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % name
-
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
-    print(url_for('hello')) # 输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli')) # 输出：/user/greyli
-    print(url_for('user_page', name='peter')) # 输出：/user/peter
-    print(url_for('test_url_for')) # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL后面。
-    print(url_for('test_url_for', num=2)) # 输出：/test?num=2
-    return 'Test page'
-    # 补充：/test 和 /test?num=2 都会报错
 
 if __name__ == '__main__':
     app.run(debug=True)
