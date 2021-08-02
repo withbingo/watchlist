181de4de585e993fabfef8f0712af4b5e3c24b12
Bingo <withbingo@126.com>
2021-08-02 23:44:50 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -14,8 +14,14 @@ else:
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+# 修改前
+# app.config['SECRET_KEY'] = 'dev'
+# 修改后
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+# 修改前
+# app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+# 修改后
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
